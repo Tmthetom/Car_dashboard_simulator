1a39db86301df5cfa1c7d77b3329579dbde3d538
Tomáš Moravec <moravec-tomas@seznam.cz>
2017-11-26 12:47:24 +0100

    Firmware implemented and tested

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,13 +1,80 @@
-void setup()
-{
+/* Setting */
 
-  /* add setup code here */
+// Communicaton
+#define baudRate 9600
+#define inBufferSize 50
 
+// Pins (PWM)
+#define speedPin 5
+#define revolutionPin 6
+#define fuelPin 10
+#define temperaturePin 11
+
+// Range of values
+#define speedMaxRange 240
+#define revolutionMaxRange 7000
+#define fuelMaxRange 100
+#define temperatureMaxRange 100
+
+/* Strings */
+
+char stringSpeed = 'S';
+char stringRevolutions = 'R';
+char stringFuel = 'F';
+char stringTemperature = 'T';
+String inMessage;
+
+/* Values */
+
+int inValue = 0;
+int pwmValue = 0;
+bool inComplete = false;
+
+/* Setup before start */
+void setup() {
+
+	// Allocation
+	inMessage.reserve(inBufferSize);
+
+	// Start communication
+	Serial.begin(baudRate);
+	Serial.println("Connected!");
 }
 
-void loop()
-{
+/* Main program loop */
+void loop() {
+	if (inComplete && inMessage.length() != 0) {
 
-  /* add main program code here */
+		// Process incoming data
+		inValue = inMessage.substring(1, inMessage.length()).toInt();  // Get value
 
+		// Show data for function
+		if (inMessage.charAt(0) == stringSpeed) setPinValue(speedPin, speedMaxRange);
+		else if (inMessage.charAt(0) == stringRevolutions) setPinValue(revolutionPin, revolutionMaxRange);
+		else if (inMessage.charAt(0) == stringFuel) setPinValue(fuelPin, fuelMaxRange);
+		else if (inMessage.charAt(0) == stringTemperature) setPinValue(temperaturePin, temperatureMaxRange);
+
+		// Flag message processed
+		inMessage = "";
+		inComplete = false;
+	}
+}
+
+/* Set speed for selected pin in selected range*/
+void setPinValue(int pin, int maxRange) {
+	pwmValue = map(inValue, 0, maxRange, 0, 255);  // Map value from clock range to PWM range
+	analogWrite(pin, pwmValue);  // Write value
+	Serial.println(pwmValue);
+}
+
+/* Serial interrput */
+void serialEvent() {
+	while (Serial.available()) {
+		char inChar = (char)Serial.read();
+		inMessage += inChar;
+		if (inChar == '\n') {
+			inMessage.trim();
+			inComplete = true;
+		}
+	}
 }
---
+++ Firmware/Release/Firmware.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\morav\AppData\Local\Temp\VMBuilds\Firmware\uno\Release\Firmware.cpp.o: \
+ C:\Users\morav\AppData\Local\Temp\VMBuilds\Firmware\uno\Release\Firmware.cpp \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programs\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
